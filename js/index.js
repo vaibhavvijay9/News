c1ad095e591b817213ca6106d57134d67f4d05f3
Vaibhav Vijay <vaibhavvijay9@gmail.com>
2017-11-12 16:08:42 +0530

    fetched data and displayed it

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 function getNews() {
-    $.ajax({
-        url: 'https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=06840ae9c7184b5b948435c6b5b41913',
-        dataType: 'jsonp',
-        success: function(data) {
+  $.ajax({
+    url:
+      "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=06840ae9c7184b5b948435c6b5b41913",
+    dataType: "json",
 
-        }
-    });
+    success: function(data) {
+      console.log(data);
+      $(".block").text(data.articles[0].title);
+      $(".block").append(data.articles[0].description);
+
+    }
+  });
 }
 getNews();
