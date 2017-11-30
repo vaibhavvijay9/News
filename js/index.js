b0c7105bbe97896749478906d0968ac6b56f19c4
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2017-11-30 22:52:51 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -1,28 +1,25 @@
 function getNews() {
-  $.ajax({
-    url:
-      "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=latest&apiKey=06840ae9c7184b5b948435c6b5b41913",
-    dataType: "json",
+    $.ajax({
+        url: "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=latest&apiKey=06840ae9c7184b5b948435c6b5b41913",
+        dataType: "json",
 
-    success: function(data) {
-      console.log(data);
-      for (var i = 0; i < data.articles.length; i++) {
-        console.log(i);
-        $(".news-content").append(
-          "<div class='z-depth-1 news-block'><div><img class='news-image' src=" +
-            data.articles[i].urlToImage +
-            " alt='Image not found'/></div><div class='news'><li><a href=" +
-            data.articles[i].url +
-            ">" +
-            data.articles[i].title +
-            "</a></li><li>" +
-            data.articles[i].description +
-            "</li><li>Published: " +
-            data.articles[i].publishedAt +
-            "</li></div></div>"
-        );
-      }
-    }
-  });
+        success: function(data) {
+            for (var i = 0; i < data.articles.length; i++) {
+                $(".news-content").append(
+                    "<div class='z-depth-1 news-block'><div><img class='news-image' src=" +
+                    data.articles[i].urlToImage +
+                    " alt='Image not found'/></div><div class='news'><li><a href=" +
+                    data.articles[i].url +
+                    ">" +
+                    data.articles[i].title +
+                    "</a></li><li>" +
+                    data.articles[i].description +
+                    "</li><li>Published: " +
+                    data.articles[i].publishedAt +
+                    "</li></div></div>"
+                );
+            }
+        }
+    });
 }
 getNews();
