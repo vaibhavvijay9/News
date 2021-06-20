1118373f9497049367b1ee6e9cf7744f2eaf3d1a
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2021-06-20 16:10:43 +0530

    api changed to Gnews, removed materializeCSS

--- js/index.js
+++ js/index.js
@@ -1,35 +1,47 @@
 function getNews() {
-    const settings = {
-        "url": "http://api.mediastack.com/v1/news?access_key=39bea7d5c8a0621ed3c1bca6730fe8d1&&languages=en",
-        "method": "GET"
-    };
+    let query = document.getElementById('search').value;
+    let settings = {};
+
+    if(query && query.length > 0) {
+        settings = {
+            "url": "https://gnews.io/api/v4/search?token=129e24acd3ee041963442e86f8dcd7d5&lang=en&q="+'"'+query+'"',
+            "method": "GET"
+        };
+    }
+    else {
+        settings = {
+            "url": "https://gnews.io/api/v4/top-headlines?token=129e24acd3ee041963442e86f8dcd7d5&lang=en",
+            "method": "GET"
+        };
+    }
 
     $.ajax(settings).done(function (response) {
-        console.log(response);
-        let articles = response.data;
+        let articles = response.articles;
+        $(".news-content").empty();
         for (var i = 0; i < articles.length; i++) {
             if(!articles[i].image)
                 articles[i].image = './news_128px.png';
-            if(articles[i].published_at)
-                articles[i].published_at = formatDate(articles[i].published_at);
+            if(articles[i].publishedAt)
+                articles[i].publishedAt = formatDate(articles[i].publishedAt);
             if(articles[i].description)
                 articles[i].description = articles[i].description.substring(0, 300);
             $(".news-content").append(
-                "<div class='z-depth-1 news-block'><div><img class='news-image' src=" +
+                "<div class='news-block'><div><img class='news-image' src=" +
                 articles[i].image +
-                " alt='Image not found'/></div><div class='news'><li><a class='news-title' href=" +
+                " alt='News'/></div><div class='news'><li><a class='news-title' href=" +
                 articles[i].url +
                 ">" +
                 articles[i].title +
                 "</a></li><li class='news-description'>" +
                 articles[i].description +
                 "</li><li><span class='published-label'>Published:</span><span class='published-value'> " +
-                articles[i].published_at +
+                articles[i].publishedAt +
                 "</span></li></div></div>"
             );
         }
     });
 }
+
 getNews();
 
 
